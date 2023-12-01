37061fbb02780180ecf4559baab58e1c8a510a72
junw <wo1261931780@gmail.com>
2023-12-01 13:00:50 +0800

    1

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # test-xxl-job
 测试xxl-job
+默认管理员账号admin/123456进行登录
--- src/main/java/wo1261931870/testxxljob/TestXxlJobApplication.java
+++ src/main/java/wo1261931870/testxxljob/TestXxlJobApplication.java
@@ -3,6 +3,9 @@ package wo1261931870.testxxljob;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author junw
+ */
 @SpringBootApplication
 public class TestXxlJobApplication {
 

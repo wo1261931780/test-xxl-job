33132742e1d247fad78e451b9f0fc9471d4293cc
junw <wo1261931780@gmail.com>
2023-12-01 12:58:33 +0800

    初始化

---
+++ src/main/java/wo1261931870/testxxljob/TestXxlJobApplication.java
@@ -0,0 +1,13 @@
+package wo1261931870.testxxljob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestXxlJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestXxlJobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wo1261931870/testxxljob/TestXxlJobApplicationTests.java
@@ -0,0 +1,13 @@
+package wo1261931870.testxxljob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestXxlJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
